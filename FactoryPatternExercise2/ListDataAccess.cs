4ca3daa6edb002e9c928aecb1c911c87d4b03f6b
2305 <npjt2305@yahoo.com>
2022-11-13 12:06:02 -0500

    ExerciseCompleted

---
+++ FactoryPatternExercise2/ListDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FactoryPatternExercise2
+{
+    public class ListDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+           new Product() { Name = "Xbox", Price = 350 },
+           new Product() { Name = "iPhone", Price = 800 },
+           new Product() { Name = "Kitten", Price = 500},
+           new Product() { Name = "Necklace", Price = 2000},
+        };
+        
+        public List<Product> LoadDate()
+        {
+            Console.WriteLine("I am reading data from List Data Access.");
+            return Products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I am saving data from List Data Access.");
+        }
+    }
+}
+}
---
+++ FactoryPatternExercise2/MongoDataAccess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FactoryPatternExercise2
+{
+    public class MongoDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+        new Product() { Name = "Xbox", Price = 350 },
+        new Product() { Name = "iPhone", Price = 800 },
+        new Product() { Name = "Kitten", Price - 500},
+        new Product() { Name = "Necklace", Price 2000},
+        };
+        
+        public List<Product> LoadDate()
+        {
+            Console.WriteLine("I am reading data from Mongo Data Access.");
+            return Products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I am saving data from Mongo Data Access.");
+        }
+    }
+}
